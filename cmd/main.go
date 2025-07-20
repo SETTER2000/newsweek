032fa9330baf4d1f8074a11e1d6a246b58a67968
SETTER <lphp@mail.ru>
2025-07-20 20:39:17 +0300

    добавлены шаблоны

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/template/html/v2"
 	slogfiber "github.com/samber/slog-fiber"
 	"github.com/setter2008/newsweek/config"
 	"github.com/setter2008/newsweek/internal/pages"
 	"github.com/setter2008/newsweek/pkg/logger"
+	"strings"
 )
 
 func main() {
@@ -14,7 +16,17 @@ func main() {
 	config.NewDatabaseConfig()
 	logConfig := config.NewLogConfig()
 	customLogger := logger.NewLogger(logConfig)
-	app := fiber.New()
+	engine := html.New("./html", ".html")
+	engine.AddFuncMap(map[string]interface{}{
+		"ToUpper": func(c string) string { return strings.ToUpper(c) },
+	})
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	// Отдача статических файлов из папки "public"
+	app.Static("/public", "./public")
 
 	app.Use(slogfiber.New(customLogger))
 	app.Use(recover.New())
--- internal/pages/pages.go
+++ internal/pages/pages.go
@@ -6,6 +6,11 @@ type Handler struct {
 	router fiber.Router
 }
 
+type Items struct {
+	ID   int
+	Name string
+}
+
 func NewHandler(router fiber.Router) {
 	h := &Handler{
 		router: router,
@@ -16,7 +21,19 @@ func NewHandler(router fiber.Router) {
 }
 
 func (h *Handler) home(c *fiber.Ctx) error {
-	return c.SendString("Hello")
+
+	names := []string{"Еда", "Животные", "Машины", "Спорт", "Музыка", "Технологии", "Прочее"}
+	items := []Items{
+		{ID: 1, Name: "Anton"},
+		{ID: 2, Name: "Vasia"},
+	}
+
+	//names := []string{"Антон", "Вася"}
+	data := struct {
+		Names []string
+		Items []Items
+	}{Names: names, Items: items}
+	return c.Render("page", data)
 }
 func (h *Handler) error(c *fiber.Ctx) error {
 	//return fmt.Errorf("Error")
