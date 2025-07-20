daf1b9dfe5f9bd7c1cc6434c17fb66faa46b883a
SETTER <lphp@mail.ru>
2025-07-20 20:41:28 +0300

    добавлены шаблоны

--- internal/pages/pages.go
+++ internal/pages/pages.go
@@ -21,21 +21,12 @@ func NewHandler(router fiber.Router) {
 }
 
 func (h *Handler) home(c *fiber.Ctx) error {
-
 	names := []string{"Еда", "Животные", "Машины", "Спорт", "Музыка", "Технологии", "Прочее"}
-	items := []Items{
-		{ID: 1, Name: "Anton"},
-		{ID: 2, Name: "Vasia"},
-	}
-
-	//names := []string{"Антон", "Вася"}
 	data := struct {
 		Names []string
-		Items []Items
-	}{Names: names, Items: items}
+	}{Names: names}
 	return c.Render("page", data)
 }
 func (h *Handler) error(c *fiber.Ctx) error {
-	//return fmt.Errorf("Error")
 	return c.SendString("Error")
 }
